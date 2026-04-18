285f67ffcc5bdc68cfd2afc3cb32b45fc766d02d
Wataru Ichimura <wata_2@icloud.com>
2026-04-19 01:56:18 +0900

    hex color vo

--- backend/domain/valueobject/hex_color.go
+++ backend/domain/valueobject/hex_color.go
@@ -1,1 +1,28 @@
 package valueobject
+
+import (
+	"regexp"
+
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type HexColor struct {
+	value string
+}
+
+var hexColorPattern = regexp.MustCompile(`^#[0-9A-Fa-f]{6}$`)
+
+func NewHexColor(s string) (*HexColor, error) {
+	err := validation.Validate(s,
+		validation.Required,
+		validation.Match(hexColorPattern),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &HexColor{value: s}, nil
+}
+
+func (h HexColor) String() string {
+	return h.value
+}
