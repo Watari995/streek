11e65bce76b5dbb7d021f6c0e0c527e0a1a00647
Wataru Ichimura <wata_2@icloud.com>
2026-04-24 20:44:24 +0900

    commit delete

--- backend/internal/application/habit/delete.go
+++ backend/internal/application/habit/delete.go
@@ -1,7 +1,37 @@
 package habit
 
-import "github.com/Watari995/streek/backend/internal/domain/repository"
+import (
+	"context"
+
+	"github.com/Watari995/streek/backend/internal/domain/repository"
+	"github.com/Watari995/streek/backend/internal/domain/valueobject"
+	"github.com/cockroachdb/errors"
+)
 
 type Delete struct {
 	habitRepo repository.IHabitRepository
+}
+
+type DeleteInput struct {
+	ID     valueobject.HabitID
+	UserID valueobject.UserID
+}
+
+func (h *Delete) Do(ctx context.Context, input DeleteInput) error {
+	habit, err := h.habitRepo.FindByID(ctx, input.ID)
+	if err != nil {
+		return errors.Wrap(err, "failed to find habit")
+	}
+	if habit.UserID() != input.UserID {
+		return errors.New("forbidden access")
+	}
+	err = h.habitRepo.Delete(ctx, input.ID)
+	if err != nil {
+		return errors.Wrap(err, "failed to delete habit")
+	}
+	return nil
+}
+
+func NewDelete(habitRepo repository.IHabitRepository) *Delete {
+	return &Delete{habitRepo: habitRepo}
 }
