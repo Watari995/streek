8075423b08ad55702d8aa1f567afa525522d942f
Wataru Ichimura <wata_2@icloud.com>
2026-04-25 14:28:57 +0900

    impl check in use case wip

--- backend/internal/application/check_in/check_in.go
+++ backend/internal/application/check_in/check_in.go
@@ -1,1 +1,49 @@
 package checkin
+
+import (
+	"context"
+	"time"
+
+	"github.com/Watari995/streek/backend/internal/apperror"
+	"github.com/Watari995/streek/backend/internal/domain/entity"
+	"github.com/Watari995/streek/backend/internal/domain/repository"
+	"github.com/Watari995/streek/backend/internal/domain/valueobject"
+)
+
+type CheckIn struct {
+	checkInRepo repository.ICheckInRepository
+	habitRepo   repository.IHabitRepository
+}
+
+type CheckInInput struct {
+	HabitID valueobject.HabitID
+	UserID  valueobject.UserID
+}
+
+func (c *CheckIn) Do(ctx context.Context, input CheckInInput) error {
+	habit, err := c.habitRepo.FindByID(ctx, input.HabitID)
+	if err != nil {
+		return apperror.NewInternalServerError().SetMessage("failed to find habit")
+	}
+	if habit == nil {
+		return apperror.NewNotFoundError().SetMessage("habit not found")
+	}
+	if habit.UserID() != input.UserID {
+		return apperror.NewForbiddenError().SetMessage("you do not have permission to check in this habit")
+	}
+
+	// create check in entity and save it
+	checkInEntity := entity.CreateCheckIn(
+		input.HabitID,
+		// now should be  JST, so must check
+		time.Now(),
+	)
+	if _, err := c.checkInRepo.Save(ctx, checkInEntity); err != nil {
+		return apperror.NewInternalServerError().SetMessage("failed to check in")
+	}
+	return nil
+}
+
+func NewCheckIn(checkInRepo repository.ICheckInRepository) *CheckIn {
+	return &CheckIn{checkInRepo: checkInRepo}
+}
