a457269eae05548b468d1d3e581a995ef58a7637
Wataru Ichimura <wata_2@icloud.com>
2026-05-05 12:34:31 +0900

    commit

--- backend/internal/domain/transaction/transaction_manager.go
+++ backend/internal/domain/transaction/transaction_manager.go
@@ -2,6 +2,10 @@ package transaction
 
 import "context"
 
+// このcontextを使ってRepositoryを呼ぶと自動的に同一transaction内で実行される
+// fnが nilを返したら commit, errorを返したら rollback
+// postgresのdeadlock検出時は最大３回までリトライする
+// すでにtxに参加している場合はそのtxを使う
 type ITransactionManager interface {
 	Run(ctx context.Context, fn func(ctx context.Context) error) error
 }
