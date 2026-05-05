740c4b4f71fa3149783a8b73fd17e642051c58c9
Wataru Ichimura <wata_2@icloud.com>
2026-05-05 16:02:01 +0900

    vo

---
+++ backend/internal/domain/valueobject/positive_int.go
@@ -0,0 +1,23 @@
+package valueobject
+
+import (
+	validation "github.com/go-ozzo/ozzo-validation/v4"
+)
+
+type PositiveInt struct {
+	value int
+}
+
+func NewPositiveInt(v int) (PositiveInt, error) {
+	err := validation.Validate(v,
+		validation.Min(1),
+	)
+	if err != nil {
+		return PositiveInt{}, err
+	}
+	return PositiveInt{value: v}, nil
+}
+
+func (p PositiveInt) Int() int {
+	return p.value
+}
